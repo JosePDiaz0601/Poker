4224af4287d46615fd659f0c6efc5cc4fa07efb3
team1 <pkhayet@uci.edu>
2022-05-10 13:13:30 -0700

    Added the basics of the decks

--- poker.h
+++ poker.h
@@ -1,1 +1,27 @@
-//Header file for poker.c
+// Header file for poker.c
+#ifndef POKERHEADER
+#define POKERHEADER
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void startGame(int players);
+
+struct DECK
+{
+    struct CARD *first = NULL;
+    struct CARD *last = NULL;
+    int length;
+};
+struct CARD
+{
+    char suit;
+    char type;
+    struct CARD *next = NULL;
+    struct CARD *prev = NULL;
+    struct DECK *deck = NULL;
+};
+
+#endif
