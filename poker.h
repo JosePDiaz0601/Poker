7798601468175d30d1a11508e1ad77d87134b192
Victor Dam <vdam1@uci.edu>
2022-05-28 18:09:38 -0700

    global var for player name

--- poker.h
+++ poker.h
@@ -13,12 +13,12 @@ void startGame(int players);
 int main(void);
 int main(int argc, char *argv[]);
 void newRound();
-char player1Name[16];
-char player2Name[16];
-char player3Name[16];
-char player4Name[16];
-char player5Name[16];
-char player6Name[16];
+char player1Name[16] = "empty";
+char player2Name[16] = "empty";
+char player3Name[16] = "empty";
+char player4Name[16] = "empty";
+char player5Name[16] = "empty";
+char player6Name[16] = "empty";
 struct CARD
 {
     char suit;
